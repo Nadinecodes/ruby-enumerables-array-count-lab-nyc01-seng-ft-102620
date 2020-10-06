b837de5ea3c68611fe455a46a89d0822b1c1c81f
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-10-06 12:44:28 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,6 @@ array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
 array_2 = ["", 4, "", "goodbye",""]
 
 def count_strings(array)
-  string = ""
   array_1.count do |array|
     array.include?""
   # Return the total number of strings in the provided array using the count enumerable
